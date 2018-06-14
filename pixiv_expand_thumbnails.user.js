d54dae59b57113068ed63509c9117da2db4638d4
Zeph <zephyruszzz@gmail.com>
2018-06-14 01:55:10 -0700

    Add waitForKeyElements

--- pixiv_expand_thumbnails.user.js
+++ pixiv_expand_thumbnails.user.js
@@ -10,10 +10,105 @@
 // @include        https://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
 // @include        https://www.pixiv.net/member_illust.php?illust_id=*&mode=medium
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require        https://gist.github.com/raw/2625891/waitForKeyElements.js
+
 
 
 // ==/UserScript==
+
+
+// https://gist.github.com/raw/2625891/waitForKeyElements.js due to unapproval @require
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+
+    Usage example:
+
+        waitForKeyElements (
+            "div.comments"
+            , commentCallbackFunction
+        );
+
+        //--- Page-specific function to do what we want when the node is found.
+        function commentCallbackFunction (jNode) {
+            jNode.text ("This comment changed by waitForKeyElements().");
+        }
+
+    IMPORTANT: This function requires your script to have loaded jQuery.
+*/
+function waitForKeyElements (
+selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+ actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+ bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+ iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
+    else
+        targetNodes     = $(iframeSelector).contents ()
+            .find (selectorTxt);
+
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                waitForKeyElements (    selectorTxt,
+                                    actionFunction,
+                                    bWaitOnce,
+                                    iframeSelector
+                                   );
+            },
+                                       300
+                                      );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
 
 
 waitForKeyElements ("figure", expandThumbnail);
@@ -44,14 +139,12 @@ function expandThumbnail () {
                 var nPages = result.getElementsByClassName('item-container').length;
 
                 if (!nPages) {
-                    //metaNode = container.parentNode.getElementsByClassName('meta')[0];
-                    //nPages = +metaNode.childNodes[1].innerHTML.split(' ')[1].replace('P','');
-                    console.debug(nPages);
+                    console.debug(illustId);
                 }
                 function retrievePages(page, srcs) {
                     var request = new XMLHttpRequest();
                     var imgSourceLink = 'https://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=' + illustId + '&page=' + page;
-                    // console.debug(imgSourceLink);
+                    console.debug(imgSourceLink);
                     request.open('GET', imgSourceLink);
                     request.onreadystatechange = function () {
                         if (request.readyState != 4 || request.status != 200) {
