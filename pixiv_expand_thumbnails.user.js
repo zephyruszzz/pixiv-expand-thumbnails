664280e5f803b5e8c07b885dc6bb8a0ef0315c8e
Zeph <zephyruszzz@gmail.com>
2018-06-15 00:38:26 -0700

    Update pixiv_expand_thumbnails.user.js
    get page number from current page.

--- pixiv_expand_thumbnails.user.js
+++ pixiv_expand_thumbnails.user.js
@@ -3,7 +3,7 @@
 // @name:ja        pixiv_expand_thumbnails
 // @name:zh-CN     pixiv_expand_thumbnails
 // @namespace      https://greasyfork.org/scripts/5480-pixiv-expand-thumbnails/
-// @version        2.0.0
+// @version        2.0.1
 // @description    Expand thumbnails and links to the original pages on pixiv.
 // @description:ja pixivのイラストページでサムネイルを各マンガページへ展開します。
 // @description:zh-CN 在Pixiv缩略图页面中显示漫画内容。
@@ -113,6 +113,23 @@ selectorTxt,    /* Required: The jQuery selector string that
 
 waitForKeyElements ("figure", expandThumbnail);
 
+function getNpage(containerNode) {
+    /*
+    <figure>
+      <div role=presentation>
+        <div>
+          <div>                  -- pagenumber
+        <div role=presentation>  -- containerNode
+      <div>                      -- comment/bookmark
+      <figcaption>               -- caption
+    */
+    return containerNode.previousSibling.firstElementChild.innerText.split('/')[1]
+}
+
+function getIllustId() {
+    return location.href.split('?')[1].split('&').filter(function (e) {return !e.indexOf('illust_id');})[0].split('=')[1];
+}
+
 function expandThumbnail () {
     var nodeFigure = document.querySelectorAll('figure')[0];
 
@@ -127,55 +144,44 @@ function expandThumbnail () {
         var linkParams = linkNode.href.split('?')[1];
 
         if (linkParams && !linkParams.indexOf('mode=manga')) {
-            var illustId = location.href.split('?')[1].split('&').filter(function (e) {return !e.indexOf('illust_id');})[0].split('=')[1];
-            var request = new XMLHttpRequest();
-            request.open('GET', location.href.replace('mode=medium', 'mode=manga'));
-            request.onreadystatechange = function () {
-                if (request.readyState != 4 || request.status != 200) {
-                    return;
-                }
-                var result = document.createElement('span');
-                result.innerHTML = request.responseText;
-                var nPages = result.getElementsByClassName('item-container').length;
-
-                if (!nPages) {
-                    console.debug(illustId);
-                }
-                function retrievePages(page, srcs) {
-                    var request = new XMLHttpRequest();
-                    var imgSourceLink = 'https://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=' + illustId + '&page=' + page;
-                    console.debug(imgSourceLink);
-                    request.open('GET', imgSourceLink);
-                    request.onreadystatechange = function () {
-                        if (request.readyState != 4 || request.status != 200) {
-                            return;
+            var illustId = getIllustId();
+            var nPages = getNpage(container);
+            if (!nPages) {
+                console.debug(nPages, illustId);
+            }
+            function retrievePages(page, srcs) {
+                var request = new XMLHttpRequest();
+                var imgSourceLink = 'https://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=' + illustId + '&page=' + page;
+                console.debug(imgSourceLink);
+                request.open('GET', imgSourceLink);
+                request.onreadystatechange = function () {
+                    if (request.readyState != 4 || request.status != 200) {
+                        return;
+                    }
+                    var result = document.createElement('span');
+                    result.innerHTML = request.responseText;
+                    var sourceImgElement = result.getElementsByTagName('img')[0];
+                    srcs[page] = sourceImgElement.src;
+                    if (page != 0) {
+                        retrievePages(page-1, srcs);
+                    } else {
+                        var html = [], h = -1;
+                        for (var i = -1, src; src = srcs[++i]; ) {
+                            html[++h] = '<a href="';
+                            html[++h] = src;
+                            html[++h] = '" target="_blank"><img style="max-width:740px;margin:20px 0 0;" src="';
+                            html[++h] = src;
+                            html[++h] = '"/></a>';
                         }
-                        var result = document.createElement('span');
-                        result.innerHTML = request.responseText;
-                        var sourceImgElement = result.getElementsByTagName('img')[0];
-                        srcs[page] = sourceImgElement.src;
-                        if (page != 0) {
-                            retrievePages(page-1, srcs);
-                        } else {
-                            var html = [], h = -1;
-                            for (var i = -1, src; src = srcs[++i]; ) {
-                                html[++h] = '<a href="';
-                                html[++h] = src;
-                                html[++h] = '" target="_blank"><img style="max-width:740px;margin:20px 0 0;" src="';
-                                html[++h] = src;
-                                html[++h] = '"/></a>';
-                            }
-                            container.innerHTML = html.join('');
-                            var att = document.createAttribute('style');
-                            att.value = "display: inline-block;overflow: scroll;text-align:center";
-                            container.setAttributeNode(att);
-                        }
-                    };
-                    request.send(null);
-                }
-                retrievePages(nPages-1, []);
-            };
-            request.send(null);
-        }
+                        container.innerHTML = html.join('');
+                        var att = document.createAttribute('style');
+                        att.value = "display: inline-block;overflow: scroll;text-align:center";
+                        container.setAttributeNode(att);
+                    }
+                };
+                request.send(null);
+            }
+            retrievePages(nPages-1, []);
+        };
     }
 }
